e5cf43761638f13d53cc0ab95803019e28c0606e
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2022-11-08 08:25:05 +0100

    redirect customer payment when account is suspended to suspend_error.html

--- staticfiles/dashboard/author/js/message.js
+++ staticfiles/dashboard/author/js/message.js
@@ -1,33 +1,32 @@
 
-const message = document.querySelector('#message');
+// const message = document.querySelector('#message');
 
-setTimeout(() => {
-    message.style.display = "none";
-}, 5000);
+// setTimeout(() => {
+//     message.style.display = "none";
+// }, 5000);
 
-document.querySelectorAll('.messages').forEach((mess) => {
-    setTimeout(() => {
-        mess.style.display = "none";
-    }, 5000);
-});
+// document.querySelectorAll('.messages').forEach((mess) => {
+//     setTimeout(() => {
+//         mess.style.display = "none";
+//     }, 5000);
+// });
 
 
-const accountSuspendBtn = document.querySelectorAll('.account_suspend_btn');
-accountSuspendBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        const suspendDiv = document.createElement('div');
-        suspendDiv.classList.add('suspend');
-        suspendDiv.setAttribute('id', 'message');
-        suspendDiv.innerText = document.querySelector('.suspend_message').value;
+// const accountSuspendBtn = document.querySelectorAll('.account_suspend_btn');
+// accountSuspendBtn.forEach((btn) => {
+//     btn.addEventListener('click', () => {
+//         const suspendDiv = document.createElement('div');
+//         suspendDiv.classList.add('suspend');
+//         suspendDiv.setAttribute('id', 'message');
+//         suspendDiv.innerText = document.querySelector('.suspend_message').value;
         
-        document.querySelector('.customer-payment-container').append(suspendDiv);
-        setTimeout(() => {
-           suspendDiv.style.display = "none";
-        }, 5000);
-        // console.log(suspendDiv);
-    }
-    )
-})
+//         document.querySelector('.customer-payment-container').append(suspendDiv);
+//         setTimeout(() => {
+//            suspendDiv.style.display = "none";
+//         }, 5000);
+//     }
+//     )
+// })
 
 
 
